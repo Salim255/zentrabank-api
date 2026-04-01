5d0b02cbc7060a35f378d40fbe1267dfe9584fd1
Salim <doshka23@gmail.com>
2026-04-01 18:00:05 +0200

    built: NotFoundException class

---
+++ src/main/java/com/zentrabank/bank_api/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.zentrabank.bank_api.exceptions;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public class NotFoundException extends ResponseStatusException {
+    public NotFoundException(String message){
+        super(HttpStatus.NOT_FOUND, message);
+    }
+}
