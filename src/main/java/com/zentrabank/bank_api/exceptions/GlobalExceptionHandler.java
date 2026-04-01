9ac1988d33bce4645b8d3ab6de6ca812dd4e7c2a
Salim <doshka23@gmail.com>
2026-04-01 18:27:07 +0200

    built: GlobalExceptionHandler

---
+++ src/main/java/com/zentrabank/bank_api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.zentrabank.bank_api.exceptions;
+
+import com.zentrabank.bank_api.config.BankApiConfigProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+// We use @ControllerAdvice to handle
+// exceptions thrown by any controller in one central place.
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private final BankApiConfigProperties config;
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    public GlobalExceptionHandler(BankApiConfigProperties config){
+        this.config = config;
+    }
+
+    private Boolean isDev() {
+        return  config.profileActive().equals("dev");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handle(Exception ex){
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        String message = "Internal server error";
+
+        if (ex instanceof ResponseStatusException rse) {
+            status = HttpStatus.valueOf(rse.getStatusCode().value());
+            message = rse.getReason();
+        }
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", "error");
+        body.put("message", message);
+        body.put("data", null);
+
+        if (isDev()){
+            body.put("stack", ex.fillInStackTrace());
+        }
+
+        logger.error("{}", body);
+        return  new ResponseEntity<>(body, status);
+    }
+}
