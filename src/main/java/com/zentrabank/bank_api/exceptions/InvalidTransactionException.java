f1589180421ea6d27123b213d75fa64f7362ee8c
Salim <doshka23@gmail.com>
2026-04-02 14:01:02 +0200

    added: InvalidTransactionException

--- src/main/java/com/zentrabank/bank_api/exceptions/InvalidTransactionException.java
+++ src/main/java/com/zentrabank/bank_api/exceptions/InvalidTransactionException.java
@@ -3,8 +3,8 @@ package com.zentrabank.bank_api.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class TransferLimitExceededException extends ResponseStatusException {
-    public TransferLimitExceededException(String message){
+public class InvalidTransactionException extends ResponseStatusException {
+    public InvalidTransactionException(String message){
         super(HttpStatus.BAD_REQUEST, message);
     }
 }
