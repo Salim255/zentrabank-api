df26f24d2bd9b37702fceda37d8e92ce802e7a07
Salim <doshka23@gmail.com>
2026-04-02 16:02:25 +0200

    built: TransactionServiceImp

---
+++ src/main/java/com/zentrabank/bank_api/modules/transaction/service/TransactionServiceImp.java
@@ -0,0 +1,9 @@
+package com.zentrabank.bank_api.modules.transaction.service;
+
+import com.zentrabank.bank_api.modules.transaction.service.TransactionService;
+
+public class TransactionServiceImp implements TransactionService {
+    public String createTransaction(){
+        return "Hello from create transaction";
+    }
+}
