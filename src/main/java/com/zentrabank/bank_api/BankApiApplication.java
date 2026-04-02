e628e9bf3d6e7d596330ce829eec2c889375d460
Salim <doshka23@gmail.com>
2026-04-02 13:31:54 +0200

    added: ForbiddenException class

--- src/main/java/com/zentrabank/bank_api/BankApiApplication.java
+++ src/main/java/com/zentrabank/bank_api/BankApiApplication.java
@@ -1,13 +1,14 @@
 package com.zentrabank.bank_api;
 
+import com.zentrabank.bank_api.config.BankApiConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(BankApiConfigProperties.class)
 @SpringBootApplication
 public class BankApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BankApiApplication.class, args);
 	}
-
 }
--- src/main/java/com/zentrabank/bank_api/config/BankApiConfigProperties.java
+++ src/main/java/com/zentrabank/bank_api/config/BankApiConfigProperties.java
@@ -1,5 +1,7 @@
 package com.zentrabank.bank_api.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
 import java.time.Duration;
 
 @ConfigurationProperties(prefix = "bank-api-secrets")
