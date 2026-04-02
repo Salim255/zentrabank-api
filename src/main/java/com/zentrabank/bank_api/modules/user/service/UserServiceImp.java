72143f163a9862d1c2a391ac25db9138e1032eff
Salim <doshka23@gmail.com>
2026-04-02 15:23:54 +0200

    added: UserServiceImp

---
+++ src/main/java/com/zentrabank/bank_api/modules/user/service/UserServiceImp.java
@@ -0,0 +1,8 @@
+package com.zentrabank.bank_api.modules.user.service;
+
+public class UserServiceImp implements UserService {
+    @Override
+    public String getUser() {
+        return "";
+    }
+}
