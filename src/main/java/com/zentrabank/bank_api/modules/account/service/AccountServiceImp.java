d88e6aa4947aedf76384b4db03259aa5d76e5f83
Salim <doshka23@gmail.com>
2026-04-02 15:48:42 +0200

    added:  AccountServiceImp

---
+++ src/main/java/com/zentrabank/bank_api/modules/account/service/AccountServiceImp.java
@@ -0,0 +1,24 @@
+package com.zentrabank.bank_api.modules.account.service;
+
+public class AccountServiceImp implements AccountService {
+    @Override
+    public String createAccount() {
+        return "";
+    }
+
+    @Override
+    public  String updateAccount(){
+        return  "";
+    }
+
+    @Override
+    public  String blockAccount(){
+        return  "";
+    }
+
+    @Override
+    public  String deleteAccount(){
+        return  "";
+    }
+
+}
--- src/main/java/com/zentrabank/bank_api/modules/auth/service/AuthServiceImp.java
+++ src/main/java/com/zentrabank/bank_api/modules/auth/service/AuthServiceImp.java
@@ -1,7 +1,5 @@
 package com.zentrabank.bank_api.modules.auth.service;
 
-import com.zentrabank.bank_api.modules.auth.service.AuthService;
-
 public class AuthServiceImp implements AuthService {
     public String register(){
         return "Hello";
