dcfb314b69a72609f192f10bbdaccb4615c655b5
Salim <doshka23@gmail.com>
2026-04-03 11:19:07 +0200

    built:RegisterResponseDto

--- src/main/java/com/zentrabank/bank_api/modules/auth/dto/RegisterResponseDto.java
+++ src/main/java/com/zentrabank/bank_api/modules/auth/dto/RegisterResponseDto.java
@@ -1,3 +1,16 @@
 package com.zentrabank.bank_api.modules.auth.dto;
 
-public record RegisterResponseDto(String message ) { }
+import java.time.Instant;
+import java.util.UUID;
+
+public record RegisterResponseDto(
+        UUID id,
+        String email,
+        String firstName,
+        String lastName,
+        String role,
+        int loginId,
+        int tempPassword,
+        Instant createdAt,
+        Instant updatedAt
+) { }
