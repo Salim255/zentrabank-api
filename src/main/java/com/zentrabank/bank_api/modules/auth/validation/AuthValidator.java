23452589aa855979f5e16a9074d689958afa65c2
Salim <doshka23@gmail.com>
2026-04-05 15:32:12 +0200

    added: InvalidEmailException

--- src/main/java/com/zentrabank/bank_api/modules/auth/validation/AuthValidator.java
+++ src/main/java/com/zentrabank/bank_api/modules/auth/validation/AuthValidator.java
@@ -1,6 +1,8 @@
 package com.zentrabank.bank_api.modules.auth.validation;
 
 import com.zentrabank.bank_api.exceptions.EmailAlreadyUsedException;
+import com.zentrabank.bank_api.exceptions.InvalidEmailException;
+import com.zentrabank.bank_api.modules.auth.dto.LoginDto;
 import com.zentrabank.bank_api.modules.auth.dto.RegisterDto;
 import com.zentrabank.bank_api.modules.auth.dto.ResetPasswordDto;
 import com.zentrabank.bank_api.modules.user.repository.UserRepository;
@@ -27,12 +29,25 @@ public class AuthValidator {
     }
 
     public void registerValidate(RegisterDto payload) {
+      try {
+          //  Validate email format
+          String email = payload.email();
+          if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+              throw new InvalidEmailException("Email format is invalid");
+          }
 
-        if (this.userRepository.existsByEmail(payload.email())) {
-            this.logger.error("Hello from in validator 👹👹👹👹👹");
-            throw new EmailAlreadyUsedException("Email already exists");
-        }
+          // Check email uniqueness
+          if (this.userRepository.existsByEmail(payload.email())) {
+              throw new EmailAlreadyUsedException("Email already exists");
+          }
+      } catch (RuntimeException ex){
+          // Log error here for debugging / monitoring
+          this.logger.error("Error during register validation: {}", ex.getMessage(), ex);
+          throw ex;
+      }
+    }
 
-        // more rules...
+    public void loginValidate(LoginDto payload){
+
     }
 }
