9039af8dc061364d3d5c37addd36113e0c3a465b
Salim <doshka23@gmail.com>
2026-04-07 19:17:08 +0200

    built: RefreshToken

---
+++ src/main/java/com/zentrabank/bank_api/modules/refreshtoken/entity/RefreshToken.java
@@ -0,0 +1,48 @@
+package com.zentrabank.bank_api.modules.refreshtoken.entity;
+
+import com.zentrabank.bank_api.modules.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Builder
+@Table(name = "refresh_tokens")
+public class RefreshToken {
+    @Id // JPA: marks this field as the primary key
+    // JPA: tells Hibernate to auto-generate the ID using a generator
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(
+            columnDefinition = "UUID", // Ensures PostgreSQL stores it as a real UUID type
+            updatable = false, // ID cannot be changed after creation
+            nullable = false  // ID must always be present
+    )
+    private UUID id;
+
+    // Link to user (many tokens per user)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    // The actual refresh token string
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    // Expiration date of the token
+    @Column(nullable = false)
+    private Instant expiresAt;
+
+    // Track if token is revoked (logout, security)
+    @Column(nullable = false)
+    private boolean revoked = false;
+
+    // When token was created
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
+}
--- src/main/java/com/zentrabank/bank_api/modules/transaction/entity/Transaction.java
+++ src/main/java/com/zentrabank/bank_api/modules/transaction/entity/Transaction.java
@@ -1,3 +1,5 @@
+package com.zentrabank.bank_api.modules.transaction.entity;
+
 import com.zentrabank.bank_api.modules.account.entity.Account.Account;
 import com.zentrabank.bank_api.modules.transaction.entity.TransactionType;
 import jakarta.persistence.*;
@@ -12,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 @Table(
         name = "transactions",
         indexes = {
