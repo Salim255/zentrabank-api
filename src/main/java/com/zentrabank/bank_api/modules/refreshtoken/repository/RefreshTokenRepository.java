544ede45af84fa8cf53633bcffecfc4d14123523
Salim <doshka23@gmail.com>
2026-04-08 00:56:31 +0200

    built: deleteByToken

--- src/main/java/com/zentrabank/bank_api/modules/refreshtoken/repository/RefreshTokenRepository.java
+++ src/main/java/com/zentrabank/bank_api/modules/refreshtoken/repository/RefreshTokenRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {}
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+    void deleteByToken(String token);
+}
