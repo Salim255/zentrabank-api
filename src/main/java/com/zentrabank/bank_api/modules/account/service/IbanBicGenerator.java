77b54f0d1581d0af6f2e5f3ccab8aa2df235dedd
Salim <doshka23@gmail.com>
2026-04-20 16:08:35 +0200

    built: randomLettersOrDigits

--- src/main/java/com/zentrabank/bank_api/modules/account/service/IbanBicGenerator.java
+++ src/main/java/com/zentrabank/bank_api/modules/account/service/IbanBicGenerator.java
@@ -126,4 +126,17 @@ public class IbanBicGenerator {
         }
         return sb.toString();
     }
+
+    // ------------------------------------------------------------
+    // HELPER: RANDOM LETTERS OR DIGITS
+    // ------------------------------------------------------------
+    private String randomLettersOrDigits(int length) {
+        // Used for BIC location code (letters or digits allowed)
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(chars.charAt(random.nextInt(chars.length())));
+        }
+        return sb.toString();
+    }
 }
