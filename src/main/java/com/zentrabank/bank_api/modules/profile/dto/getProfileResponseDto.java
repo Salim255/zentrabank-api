ade38e406c40bce7c73daa497965522b54702388
Salim <doshka23@gmail.com>
2026-04-20 15:15:03 +0200

    built: getProfileResponseDto

---
+++ src/main/java/com/zentrabank/bank_api/modules/profile/dto/getProfileResponseDto.java
@@ -0,0 +1,6 @@
+package com.zentrabank.bank_api.modules.profile.dto;
+
+import com.zentrabank.bank_api.modules.profile.dto.ProfileDto;
+
+public record getProfileResponseDto(ProfileDto profile) {
+}
