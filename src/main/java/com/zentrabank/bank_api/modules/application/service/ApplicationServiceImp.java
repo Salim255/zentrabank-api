0cb4d48dbc96e042de45dd8f2cb59881e478a89b
Salim <doshka23@gmail.com>
2026-04-21 09:41:01 +0200

    built: ApplicationServiceImp

---
+++ src/main/java/com/zentrabank/bank_api/modules/application/service/ApplicationServiceImp.java
@@ -0,0 +1,9 @@
+package com.zentrabank.bank_api.modules.application.service;
+
+import com.zentrabank.bank_api.modules.application.service.ApplicationService;
+
+public class ApplicationServiceImp implements ApplicationService {
+    public String createApplication(String userId){
+        return userId;
+    }
+}
