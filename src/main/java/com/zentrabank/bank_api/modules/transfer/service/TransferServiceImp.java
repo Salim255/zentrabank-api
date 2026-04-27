4731e705a96b6890b86b6e6dcabd7184b6dd3e9c
Salim <doshka23@gmail.com>
2026-04-27 22:18:36 +0200

    added: TransferServiceImp

---
+++ src/main/java/com/zentrabank/bank_api/modules/transfer/service/TransferServiceImp.java
@@ -0,0 +1,8 @@
+package com.zentrabank.bank_api.modules.transfer.service;
+
+import com.zentrabank.bank_api.modules.transfer.service.TransferService;
+import org.springframework.stereotype.Service;
+
+@Service
+public  class TransferServiceImp implements TransferService {
+}
